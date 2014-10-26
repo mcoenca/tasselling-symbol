52ae688a78e6bc1e2144522c8be93a7b33ae12b8
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2014-10-26 13:33:18 -0400

    Added search and reviews for rotten tomatoes

---
+++ rotten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import requests
+from rottentomatoes_api_key import RT_KEY
+
+RT_URL_BASE = 'http://api.rottentomatoes.com/api/public/v1.0/'
+RT_URL_SEARCH = RT_URL_BASE + 'movies.json' 
+RT_URL_REVIEWS = RT_URL_BASE + 'movies/{}/reviews.json'
+
+def get(url, **params):
+    params['apikey'] = RT_KEY
+    return requests.get(url, params=params).json()
+
+def search(query, page=1, page_limit=50):
+    return get(
+        RT_URL_SEARCH,
+        q=query,
+        page=page,
+        page_limit=page_limit)
+
+def reviews(movie_id, review_type='all', country='us', page=1, page_limit=50):
+    url = RT_URL_REVIEWS.format(movie_id)
+    return get(
+        url,
+        review_type=review_type,
+        country=country,
+        page=page,
+        page_limit=page_limit)
+
+def main():
+    print(search('shawshank'))
+    print(reviews(12989))
+
+if __name__ == '__main__':
+    main()
