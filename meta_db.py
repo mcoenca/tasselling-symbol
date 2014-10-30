f50af402c4b993c5fec8cde4a827cb1386bf8a18
Evan Palmer <evansp01@gmail.com>
2014-10-30 05:35:32 -0400

    Added definitions for meta.db

---
+++ meta_db.py
@@ -0,0 +1,41 @@
+from peewee import *
+
+db = SqliteDatabase('meta.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Movie(BaseModel):
+    id = PrimaryKeyField()
+    avguserscore = IntegerField(null=True)
+    genre = CharField(max_length=255)
+    score = IntegerField(null=True)
+    rating = CharField()
+    cast = CharField(max_length=255)
+    runtime = CharField(max_length=255)
+    url = CharField(max_length=255)
+    name = CharField(max_length=255, unique=True)
+    rlsdate = CharField(max_length=255)
+    userreviews = IntegerField(null=True)
+    criticreviews = IntegerField(null=True)
+
+class UserReview(BaseModel):
+    date = CharField(max_length=255, null=True)
+    total_thumbs = IntegerField()
+    review = TextField()
+    name = CharField(max_length=255, null=True)
+    total_ups = IntegerField()
+    score = IntegerField()
+    movie = ForeignKeyField(Movie, related_name='user_reviews')
+
+class CriticReview(BaseModel):
+    link = CharField(max_length=255, null=True)
+    critic = CharField(max_length=255, null=True)
+    excerpt = TextField()
+    score = IntegerField()
+    movie = ForeignKeyField(Movie, related_name='critic_reviews')
+
+
+db.connect()
+db.create_tables([Movie, UserReview, CriticReview], safe=True)
