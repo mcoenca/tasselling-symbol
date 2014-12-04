e0f9d2e3d1e9239e14aa8f4485eefcee74a4c06c
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2014-12-03 21:16:14 -0500

    Thoroughly improved rotten_collab

--- rotten_collab.py
+++ rotten_collab.py
@@ -3,29 +3,80 @@
 import sys
 import pickle
 import argparse
+import random
 import rotten_db as db
 import numpy as np
 from scipy import sparse
 from numpy import linalg
 
+#----------------------------------------------------------#
+# The class that actually performs collaborative filtering #
+#----------------------------------------------------------#
+
 class RottenLearn():
 
-    def __init__(self, critics, movies, scores, method='cos'):
-        self.method       = method
-        self.coo          = sparse.coo_matrix((scores, (critics, movies)))
-        self.num_critics  = self.coo.shape[0]
-        self.num_movies   = self.coo.shape[1]
+    def __init__(self, num_critics, num_movies, reviews, method='cos'):
+
+        self.method      = method
+        self.num_critics = num_critics
+        self.num_movies  = num_movies
+
+        self.coo = self._make_coo(reviews) 
+
         self.matrix       = self.coo.todense()
         self.csr          = self.coo.tocsr()
         self.csc          = self.coo.tocsc()
         self.critic_means = self._calc_critic_means()
         self.critic_sim   = self._calc_critic_sim()
 
-    def _calc_critic_means(self):
-        return np.divide(
-                self.csr.sum(axis=1),
-                self.csr.getnnz(axis=1).reshape(-1,1))
+        self.movie_reviewers, self.movie_reviews = self._calc_movie_reviews()
 
+    def _unzip_review_tuples(self, reviews):
+        critics = []
+        movies  = []
+        scores  = []
+        dedup = set()
+        n_dup = 0
+
+        for c, m, s in reviews:
+            if (c,m) in dedup:
+                #print("already seen {{c:{} m:{} s:{}}}".format(c,m,s))
+                n_dup += 1
+                continue
+            else:
+                dedup.add((c,m))
+            critics.append(c-1)
+            movies.append(m-1)
+            scores.append(s)
+
+        print("Found {} duplicate reviews".format(n_dup))
+        return critics, movies, scores
+
+    def _make_coo(self, reviews):
+        critics, movies, scores = self._unzip_review_tuples(reviews)
+        return sparse.coo_matrix(
+                (scores, (critics, movies)),
+                shape=(self.num_critics, self.num_movies))
+
+    def _calc_critic_means(self):
+        n_rev_per_critic = np.maximum(1, self.csr.getnnz(axis=1).reshape(-1,1))
+        return np.divide(self.csr.sum(axis=1), n_rev_per_critic)
+
+    def _calc_movie_reviews(self):
+        movie_reviews   = [None] * self.num_movies
+        movie_reviewers = [None] * self.num_movies
+
+        for m in range(self.num_movies):
+            r = self.csc[:, m]
+            nz = r.nonzero()
+            movie_reviewers[m] = nz[0]
+            movie_reviews[m] = r[nz]
+
+        return movie_reviewers, movie_reviews
+
+    # Calculate the dot products of the matrix rows,
+    # divided by the product of the norms
+    # Set the diagonal to zero
     def _calc_normalized_dot_products(self, csrmat):
         simil = csrmat.dot(csrmat.transpose()).todense()
         normvec = np.maximum(np.sqrt(np.diag(simil)), 1)
@@ -35,6 +86,7 @@ class RottenLearn():
             ret[i,i] = 0
         return ret
 
+    # Calculate the similarity between critics using Pearson correlation
     def _calc_critic_sim_pearson(self):
         zeromean = self.csr.copy()
         for critic_id in range(self.num_critics):
@@ -46,8 +98,9 @@ class RottenLearn():
         zeromean.eliminate_zeros()
         return self._calc_normalized_dot_products(zeromean)
 
+    # Calculate the similarity between critics using dot product (cosine)
     def _calc_critic_sim_cos(self):
-        return self_calc_normalized_dot_products(self.csr)
+        return self._calc_normalized_dot_products(self.csr)
 
     def _calc_critic_sim(self):
         if self.method == 'cosine':
@@ -60,45 +113,58 @@ class RottenLearn():
         return self.critic_sim[i,j]
 
     def estimate_rating(self, critic_id, movie_id):
-        movie_ratings = self.csc[:, movie_id]
-        nonzero = movie_ratings.nonzero()
-        sim = self.critic_sim[critic_id, nonzero[0]]
-        sim[critic_id] = 0
+        sim = self.critic_sim[critic_id, self.movie_reviewers[movie_id]]
         totsim = np.sum(sim)
 
         if totsim == 0:
             return self.critic_means[critic_id]
 
-        return np.sum(
-                np.multiply(movie_ratings[nonzero], sim)
-                ) / totsim
+        tmp = np.multiply(self.movie_reviews[movie_id], sim)
+        return np.sum(tmp) / totsim
 
-    def estimate_all_ratings(self):
-        ret = np.zeros((self.num_critics, self.num_movies))
+    def estimate_all_ratings(self, est):
+        for m in range(self.num_movies):
+            print("\rEstimating all ratings for movie {}".format(m), end='')
+            for c in range(self.num_critics):
+                if estimates[c,m] != 0:
+                    continue # already estimated
+                est[c,m] = self.estimate_rating(c,m)
+        print()
+        return est
 
-        for movie_id in range(self.num_movies):
-            print("\rEstimating all ratings for movie {}".format(movie_id),
-                    end='')
-            movie_ratings = self.csc[:, movie_id]
-            nonzero = movie_ratings.nonzero()
-            nnz = len(nonzero[0])
-            mrnz = movie_ratings[nonzero]
+    def compare_test_ratings(self, r_test):
+        c_test, m_test, s_test = self._unzip_review_tuples(r_test)         
+        n = len(c_test)
+        estimates = self.coo.todense()
+        errors = np.zeros(n)
 
-            for critic_id in range(self.num_critics):
-                sim = self.critic_sim[critic_id, nonzero[0]]
-                totsim = np.sum(sim)
-
-                if totsim == 0:
-                    ret[critic_id, movie_id] = self.critic_means[critic_id]
-                else:
-                    ret[critic_id, movie_id] = (np.sum(np.multiply(mrnz, sim))
-                        / totsim)
+        for i in range(n):
+            print("\rEstimating rating {}/{}".format(i+1,n), end='')
+            c = c_test[i]
+            m = m_test[i]
+            s = s_test[i]
+            e = self.estimate_rating(c,m)
+            estimates[c,m] = e
+            errors[i] = abs(e-s)
 
         print()
-        return ret
+        err_mean = errors.mean()
+        err_std = errors.std()
+        return err_mean, err_std, estimates
 
-def get_reviews():
-    reviews = (db.Review
+#-----------------------------------#
+# Getting stuff out of the database #
+#-----------------------------------#
+
+def shuffle_split(l, p):
+    n = len(l)
+    m = int(n * p)
+    random.shuffle(l)
+    return l[:m], l[m:]
+
+    
+def get_reviews(p):
+    reviews = list(db.Review
         .select(
             db.Review.critic,
             db.Review.movie,
@@ -106,74 +172,106 @@ def get_reviews():
         .where(db.Review.is_top == False)
         .tuples())
 
-    critics = []
-    movies = []
-    scores = []
-    dedup = set()
-    n_dup = 0
-    for c, m, s in reviews:
-        if (c,m) in dedup:
-            #print("already seen {{c:{} m:{} s:{}}}".format(c,m,s))
-            n_dup += 1
-            continue
-        else:
-            dedup.add((c,m))
-        critics.append(c-1)
-        movies.append(m-1)
-        scores.append(s)
+    return shuffle_split(reviews, p)
 
-    print("Found {} duplicate reviews".format(n_dup))
-    return critics, movies, scores
+def get_db_data_size():
+    num_critics = db.Critic.select().count()
+    num_movies = db.Movie.select().count()
+    return num_critics, num_movies
 
-def calculate_estimates(filename, method):
+#-------------------------------------------------------------#
+# Train the collaborative filtering model on part of the data #
+#-------------------------------------------------------------#
+
+def train(filename, method, p):
+    num_critics, num_movies = get_db_data_size()
+
     print("Getting reviews from database")
-    critics, movies, scores = get_reviews()
+    r_train, r_test = get_reviews(p)
+    
     print("Calculating critic similarity")
-    learn = RottenLearn(critics, movies, scores, method=method)
-    print("Estimating all ratings")
-    estimates = learn.estimate_all_ratings()
-    print("Dumping estimates to {}".format(filename))
+    learn = RottenLearn(num_critics, num_movies, r_train, method=method)
+
+    data = {
+        "learn":     learn,
+        "r_train":   r_train,
+        "r_test":    r_test,
+        "estimates": sparse.coo_matrix((num_critics, num_movies))
+    }
+
+    print("Dumping data to {}".format(filename))
     with open(filename, "wb") as f:
-        pickle.dump(estimates, f)
-    print("It's all cool bro")
+        pickle.dump(data, f)
+
+    print("All done boss!")
+
+#------------------------------------#
+# Calculate missing estimate reviews #
+#------------------------------------#
+
+def calculate_all_estimates(filename):
+    print("Loading data from {}".format(filename))
+    with open(filename, "rb") as f:
+        data = pickle.load(f)
+
+    learn     = data["learn"]
+    estimates = data["estimates"]
+
+    print("Estimating all remaining ratings")
+    learn.estimate_all_ratings(estimates) 
+
+    print("Dumping data to {}".format(filename))
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+
+    print("All cool bro!")
+
+#--------------------------------------#
+# Compare estimates to observed values #
+#--------------------------------------#
 
 def compare_estimates(filename):
-    print("Getting reviews from database")
-    critics, movies, scores = get_reviews()
-    print("Loading estimates from {}".format(filename))
+    print("Loading data from {}".format(filename))
     with open(filename, "rb") as f:
-        estimates = pickle.load(f)
+        data = pickle.load(f)
+
+    learn  = data["learn"]
+    r_test = data["r_test"]
+
     print("Calculating error statistics")
-    n = len(critics)
-    errors = np.zeros(n)
-    n_absurd = 0
-    for i in range(len(critics)):
-        c = critics[i]
-        m = movies[i]
-        s = scores[i]
-        e = estimates[c,m]
-        errors[i] = abs(e - s)
-        if errors[i] > 100:
-            print("critic:{} movie:{} score:{} estimate:{}".format(c, m, s, e))
-            n_absurd += 1
-    err_mean = errors.mean()
-    err_std = errors.std()
+    err_mean, err_std, estimates = learn.compare_test_ratings(r_test)
     print("Error stats: mean = {}  std dev = {}".format(err_mean, err_std))
-    print("Absurd estimates: {}".format(n_absurd))
+
+    data["estimates"] = estimates
+
+    print("Dumping data to {}".format(filename))
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+
+    print("All finished pal!") 
+
+#-------------------#
+# Actually do stuff #
+#-------------------#
 
 def main():
     parser = argparse.ArgumentParser(description='Collaborative filtering yo!')
     parser.add_argument('command', metavar='COMMAND',
-            choices=["estimate", "compare"], help='Whatchu wanna do?')
-    parser.add_argument('-f', metavar='FILE', default='collab.out',
-            help='estimates file')
+            choices=["train", "estimate", "compare"],
+            help='Whatchu wanna do: train, estimate, compare')
+    parser.add_argument('-f', metavar='FILE', default='collab.pickle',
+            help='File to load data from and dump data to')
     parser.add_argument('-m', metavar='METHOD',
             choices=['pearson', 'cosine'], default='cosine',
-            help='collaborative filtering similarity measurement method')
+            help='Similarity measurement method: cosine, pearson')
+    parser.add_argument('-p', metavar='PROPORTION', type=float,
+            help='Proportion of data to use as training samples')
 
     args = parser.parse_args()
+    if args.command == 'train':
+        train(args.f, args.m, args.p)
     if args.command == 'estimate':
-        calculate_estimates(args.f, args.m)
+        calculate_all_estimates(args.f)
     elif args.command == 'compare':
         compare_estimates(args.f)
 
